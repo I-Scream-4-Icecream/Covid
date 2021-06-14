f13b955d52d2b18fb8eebe829ec4171cf4e787e5
Remi <remikassouah@gmail.com>
2021-06-14 14:59:54 +0300

    Changed the structure of the app, seperated the stats from home.dart

--- lib/stats/stats.dart
+++ lib/stats/stats.dart
@@ -1,3 +1,6 @@
+import 'package:covid/stats/view/deaths.dart';
+import 'package:covid/stats/view/infected.dart';
+import 'package:covid/stats/view/recovered.dart';
 import 'package:flutter/material.dart';
 
 class Stats extends StatefulWidget {
@@ -11,60 +14,9 @@ class _StatsState extends State<Stats> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget> [
-        Column(
-          children: <Widget> [
-            Text(
-              'Infected',
-              style: TextStyle(
-                color: Colors.amber[200],
-                fontSize: 24.0
-              )
-            ),
-            Text(
-              '13538',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 32.0
-              )
-            ),
-          ]
-        ),
-        Column(
-          children: <Widget> [
-            Text(
-              'Recovered',
-              style: TextStyle(
-                color: Colors.green[300],
-                fontSize: 24.0
-              )
-            ),
-            Text(
-              '2450',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 32.0
-              )
-            ),
-          ]
-        ),
-        Column(
-          children: <Widget> [
-            Text(
-              'Deaths',
-              style: TextStyle(
-                color: Colors.red[300],
-                fontSize: 24.0
-              )
-            ),
-            Text(
-              '3657',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 32.0
-              )
-            ),
-          ]
-        ),
+        Infected(),
+        Recovered(),
+        Deaths()
       ]
     );
   }
